6a54361347c852613cb77123ea885fafe9dc23ee
fiona <fiona.mburu@student.moringaschool.com>
2024-08-09 06:15:32 +0300

    adds new transaction

--- src/components/NewTransaction.jsx
+++ src/components/NewTransaction.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function NewTransaction() {
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState("");
+  const handleDate = (event) => setDate(event.target.value);
+  const handleDescription = (event) => setDescription(event.target.value);
+  const handleCategory = (event) => setCategory(event.target.value);
+  const handleAmount = (event) => setAmount(event.target.value);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const transactionData = {
+      date: date,
+      description: description,
+      category: category,
+      amount: amount,
+    };
+    fetch("http://localhost:3000/transactions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(transactionData),
+    })
+      .then((res) => res.json())
+      .then((data) => data("successful"))
+      .catch((error) => console.log(error));
+    setDate("");
+    setDescription("");
+    setCategory("");
+    setAmount("");
+  };
   return (
-    <div>NewTransaction</div>
-  )
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="date"
+            placeholder="Display date"
+            value={date}
+            onChange={handleDate}
+          ></input>
+        </div>
+        <div>
+          <input
+            type="text"
+            value={description}
+            onChange={handleDescription}
+          ></input>
+        </div>
+        <div>
+          <input type="text" value={category} onChange={handleCategory} />
+        </div>
+        <div>
+          <input type="number" value={amount} onChange={handleAmount} />
+        </div>
+        <div>
+          <input type="submit" value="Add Transaction" />
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default NewTransaction
+export default NewTransaction;
